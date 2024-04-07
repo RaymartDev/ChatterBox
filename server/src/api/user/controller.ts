81ded9b6fa218b1e37aa13e5d27762ec8ddd59e4
Raymart <raymart.sarmiento23@outlook.com>
2024-04-08 01:23:30 +0800

    Clear Git Cache

--- server/src/api/user/controller.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-export const sampleController = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    res.json({ message: 'User Routes' });
-  } catch (err) {
-    next(err);
-  }
-};
--- server/src/api/user/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as Controller from './controller';
-
-const router = express.Router();
-
-router.get('/', Controller.sampleController);
-
-export default router;

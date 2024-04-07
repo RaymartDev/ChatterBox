cf125c7828180fd0e03aab89ca9a7f443846a6d8
Raymart <raymart.sarmiento23@outlook.com>
2024-04-08 01:52:59 +0800

    Added welcome message for mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Welcome from './components/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Welcome />
     </View>
   );
 }
---
+++ mobile/components/Welcome.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native"
+
+export default function Welcome() {
+    return <Text>Hello ChatterBox</Text>
+}
